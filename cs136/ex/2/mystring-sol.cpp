afd1591aa44e3f42859d644c4617d9594af39a46
Alexey Nikolaev <orthofish@gmail.com>
2016-12-14 01:10:57 -0500

    ex2 upd

--- cs136/ex/2/mystring-sol.cpp
+++ cs136/ex/2/mystring-sol.cpp
@@ -7,62 +7,62 @@ using namespace std;
 
 class MyString {
 private:
-  vector<char> data;
+	vector<char> data;
 
 public:
-  int size(); // Returns the number of characters in it
-  void append(string s); // Append the characters from s at the end of the current MyString
-  void operator+= (string s); // A "fancy" way to do append
+	int size(); // Returns the number of characters in it
+	void append(string s); // Append the characters from s at the end of the current MyString
+	void operator+= (string s); // A "fancy" way to do append
 
-  // Stream input and output operators
-  friend ostream & operator<< (ostream &os, MyString t);
-  friend istream & operator>> (istream &is, MyString &t);
+	// Stream input and output operators
+	friend ostream & operator<< (ostream &os, MyString t);
+	friend istream & operator>> (istream &is, MyString &t);
 };
 
 int MyString::size() {
-  return data.size();
+	return data.size();
 }
 
 void MyString::append(string s) {
-  for(int i = 0; i < s.size(); i++) {
-    data.push_back(s[i]);
-  }
+	for(int i = 0; i < s.size(); i++) {
+		data.push_back(s[i]);
+	}
 }
 
 void MyString::operator+= (string s) {
-  this->append(s);
+	this->append(s);
 }
 
 ostream & operator<< (ostream &os, MyString t) {
-  for (int i = 0; i < t.data.size(); i++) {
-    os << t.data[i];
-  }
-  return os;
+	for (int i = 0; i < t.data.size(); i++) {
+		os << t.data[i];
+	}
+	return os;
 }
 
 istream & operator>> (istream &is, MyString &t) {
-  string s;
+	string s;
 
-  is >> s;
-  t.data.clear();
-  t.append(s);
+	is >> s;
+	t.data.clear();
+	t.append(s);
 
-  return is;
+	return is;
 }
 
 int main() {
-  MyString s;      // Default constructor is called, makes an empty MyString
-  s.append("ABC"); // "ABC" gets appended to it
-  s += "DEF";      // and "DEF" too.
+	MyString s;      // Default constructor is called, makes an empty MyString
+	s.append("ABC"); // "ABC" gets appended to it
+	s += "DEF";      // and "DEF" too.
 
-  cout << s.size() << endl; // Prints "6" and end-of-line
-  cout << s << endl; // Prints "ABCDEF" and end-of-line
+	cout << s.size() << endl; // Prints "6" and end-of-line
+	cout << s << endl; // Prints "ABCDEF" and end-of-line
 
-  // We would also want to read words from a text file,
-  // the same way the type `string` would do that:
-  fstream fin("file.txt");
-  while(fin >> s) {
-    cout << "[" << s << "]" << endl;
-  }
-  fin.close();
+	// We would also want to read words from a text file,
+	// the same way the type `string` would do that:
+	fstream fin("file.txt");
+	while(fin >> s) {
+		cout << "[" << s << "]" << endl;
+	}
+	fin.close();
 }
--- cs136/ex/2/mystring.cpp
+++ cs136/ex/2/mystring.cpp
@@ -7,21 +7,21 @@ using namespace std;
 
 class MyString {
 private:
-  vector<char> data;
+	vector<char> data;
 
 public:
-  int size(); // Returns the number of characters in it
-  void append(string s); // Append the characters from s at the end of the current MyString
-  void operator+= (string s); // A "fancy" way to do append
+	int size(); // Returns the number of characters in it
+	void append(string s); // Append the characters from s at the end of the current MyString
+	void operator+= (string s); // A "fancy" way to do append
 
-  // Stream input and output operators
-  friend ostream & operator<< (ostream &os, MyString t);
-  friend istream & operator>> (istream &is, MyString &t);
+	// Stream input and output operators
+	friend ostream & operator<< (ostream &os, MyString t);
+	friend istream & operator>> (istream &is, MyString &t);
 };
 
 /* Implementation: */
 int MyString::size() {
-  return 0;
+	return 0;
 }
 
 void MyString::append(string s) {
@@ -33,26 +33,26 @@ void MyString::operator+= (string s) {
 }
 
 ostream & operator<< (ostream &os, MyString t) {
-  return os;
+	return os;
 }
 
 istream & operator>> (istream &is, MyString &t) {
-  return is;
+	return is;
 }
 
 int main() {
-  MyString s;      // Default constructor is called, makes an empty MyString
-  s.append("ABC"); // "ABC" gets appended to it
-  s += "DEF";      // and "DEF" too.
+	MyString s;      // Default constructor is called, makes an empty MyString
+	s.append("ABC"); // "ABC" gets appended to it
+	s += "DEF";      // and "DEF" too.
 
-  cout << s.size() << endl; // Prints "6" and end-of-line
-  cout << s << endl; // Prints "ABCDEF" and end-of-line
+	cout << s.size() << endl; // Prints "6" and end-of-line
+	cout << s << endl; // Prints "ABCDEF" and end-of-line
 
-  // We would also want to read words from a text file,
-  // the same way the type `string` would do that:
-  fstream fin("file.txt");
-  while(fin >> s) {
-    cout << "[" << s << "]" << endl;
-  }
-  fin.close();
+	// We would also want to read words from a text file,
+	// the same way the type `string` would do that:
+	fstream fin("file.txt");
+	while(fin >> s) {
+		cout << "[" << s << "]" << endl;
+	}
+	fin.close();
 }
